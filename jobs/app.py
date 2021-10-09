3a519329472f88e8d7461399068712d1dd7f6e86
LuisMi <luis.miguel.almaraz@ericsson.com>
2021-10-09 19:30:32 +0200

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -57,7 +57,7 @@ def review(employer_id):
         rating = request.form['rating']
         title = request.form['title']
         status = request.form['status']
-        date = datetime.now().strftime("%m/%d/%Y")
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',(review, rating, title, date, status, employer_id), commit=True)
-        return redirect(url_for(employer_id=employer_id))
+        return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
